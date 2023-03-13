76959551376533798bea86a532855c9cf7678463
Roshni <rsurpur@andrew.cmu.edu>
2023-03-13 12:26:21 -0400

    edited string

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         }
       />
       <div>
-        {QueryProcessor(query) == "" ? "No Match" : QueryProcessor(query)}
+        {QueryProcessor(query) == "" ? "No Matches" : QueryProcessor(query)}
       </div>
     </div>
   );
