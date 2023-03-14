855b47ad5bc944c265f4e8e9b96c28648623e7d5
Roshni <rsurpur@andrew.cmu.edu>
2023-03-14 15:55:03 -0400

    pushed

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,7 +6,7 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
-  if (query.toLowerCase().includes("What is your name?")) {
+  if (query.toLowerCase().includes("name")) {
     return (
       "Your name" 
     );
